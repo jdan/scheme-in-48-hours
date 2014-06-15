3322f039e0f8926d0c703f37f63ec19029a6493b
Jordan Scales <scalesjordan@gmail.com>
2014-06-15 15:01:29 -0400

    added eval support for primitive functions

--- parser.hs
+++ parser.hs
@@ -102,6 +102,39 @@ eval val@(Bool _) = val
 -- here, val is `nested`
 eval (List [Atom "quote", val]) = val
 
+-- eval simple functions
+-- extract func and args from a list expression
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+-- lookup function in a list of primitives
+-- if it isn't found, return #f
+-- if it is found, apply it to args
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+-- weak typing: (* "2" 2) = 4
+-- uses the built-in `reads`
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                             then 0
+                             else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 main :: IO ()
 main = getArgs >>= print . eval . readExpr . head
